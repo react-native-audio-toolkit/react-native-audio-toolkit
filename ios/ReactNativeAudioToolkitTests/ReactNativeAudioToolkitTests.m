c84ea1d39d4cd7b696b51de861e507de9801d747
Oskar Vuola <ovuola@gmail.com>
2016-07-01 14:28:05 +0300

    Rename ios tests folder

--- ios/ReactNativeAudioToolkitTests/ReactNativeAudioToolkitTests.m
+++ ios/ReactNativeAudioToolkitTests/ReactNativeAudioToolkitTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface ReactNativeAudioToolkit : XCTestCase
+@interface ReactNativeAudioToolkitTests : XCTestCase
 
 @end
 
-@implementation ReactNativeAudioToolkit
+@implementation ReactNativeAudioToolkitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
