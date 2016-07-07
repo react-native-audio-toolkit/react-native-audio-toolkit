2122ceca26780f98527cd076db53ad4bb1a47e56
Rasmus Eskola <fruitiex@gmail.com>
2016-07-07 15:53:44 +0300

    Identical index.ios.js and index.android.js for now

--- ExampleApp/index.ios.js
+++ ExampleApp/index.ios.js
@@ -1,53 +1,129 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  NativeModules,
+  DeviceEventEmitter
 } from 'react-native';
 
+import {
+  RCTAudioPlayer,
+  RCTAudioRecorder
+} from 'react-native-audio-toolkit';
+
+let audioFilename = 'test.mp4';
+
 class ExampleApp extends Component {
+
+  state = {
+    recording: false,
+    playing: false
+  };
+
+  componentWillMount() {
+    DeviceEventEmitter.addListener('RCTAudioRecorder:start', (e: Event) => {console.log(e);});
+    DeviceEventEmitter.addListener('RCTAudioRecorder:ended', (e: Event) => {
+      console.log(e);
+
+      this.setState({recording: false});
+    });
+    DeviceEventEmitter.addListener('RCTAudioRecorder:error', (e: Event) => {console.log(e);});
+
+    DeviceEventEmitter.addListener('RCTAudioRecorder:info', (e: Event) => {console.log(e);});
+
+    DeviceEventEmitter.addListener('RCTAudioPlayer:playing', (e: Event) => {console.log(e);});
+    DeviceEventEmitter.addListener('RCTAudioPlayer:ended', (e: Event) => {
+      console.log(e);
+
+      this.setState({playing: false});
+    });
+    DeviceEventEmitter.addListener('RCTAudioPlayer:pause', (e: Event) => {console.log(e);});
+    DeviceEventEmitter.addListener('RCTAudioPlayer:play', (e: Event) => {console.log(e);});
+    DeviceEventEmitter.addListener('RCTAudioPlayer:error', (e: Event) => {console.log(e);});
+
+    DeviceEventEmitter.addListener('RCTAudioPlayer:info', (e: Event) => {console.log(e);});
+  }
+
+  componentDidMount() {
+  }
+
+  _stop() {
+    if (this.state.recording) {
+      RCTAudioRecorder.stop();
+      this.setState({recording: false});
+    } else if (this.state.playing) {
+      RCTAudioPlayer.stop();
+      this.setState({playing: false});
+    }
+  }
+
+  _record() {
+    if (this.state.playing) {
+      this._stop();
+    }
+
+    RCTAudioRecorder.recordLocal(audioFilename);
+    this.setState({recording: true});
+  }
+
+ _play() {
+    if (this.state.recording) {
+      this._stop();
+    }
+
+    RCTAudioPlayer.playLocal(audioFilename);
+    this.setState({playing: true});
+  }
+
+  _renderButton(title, onPress, active) {
+    var style = (active) ? styles.activeButtonText : styles.buttonText;
+
+    return (
+      <TouchableHighlight style={styles.button} onPress={onPress}>
+        <Text style={style}>
+          {title}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.controls}>
+          {this._renderButton("RECORD", () => {this._record()}, this.state.recording )}
+          {this._renderButton("STOP", () => {this._stop()} )}
+          {this._renderButton("PLAY", () => {this._play()}, this.state.playing )}
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#2b608a",
+  },
+  controls: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1,
   },
-  welcome: {
+  button: {
+    padding: 20
+  },
+  buttonText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: "#fff"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  activeButtonText: {
+    fontSize: 20,
+    color: "#B81F00"
+  }
 });
 
 AppRegistry.registerComponent('ExampleApp', () => ExampleApp);
