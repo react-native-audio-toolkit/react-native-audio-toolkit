4c3e12a19a3b061458d426756f20c37f391b69fa
Rasmus Eskola <fruitiex@gmail.com>
2017-11-01 14:24:39 +0200

    Fix Android compilation on RN 0.49.5

--- android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPackage.java
+++ android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPackage.java
@@ -19,11 +19,6 @@ public class AudioPackage implements ReactPackage {
         return modules;
     }
 
-    // Deprecated in RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
