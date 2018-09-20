d6d0836000031385ab088dd06e0c33a786f8469a
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-09-20 16:49:18 -0700

    Fix: resume recording in Android

--- android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioRecorderModule.java
+++ android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioRecorderModule.java
@@ -36,6 +36,7 @@ public class AudioRecorderModule extends ReactContextBaseJavaModule implements
 
     Map<Integer, MediaRecorder> recorderPool = new HashMap<>();
     Map<Integer, Boolean> recorderAutoDestroy = new HashMap<>();
+    Map<Integer, Boolean> recorderPaused = new HashMap<>();
 
     private ReactApplicationContext context;
 
@@ -147,6 +148,7 @@ public class AudioRecorderModule extends ReactContextBaseJavaModule implements
             recorder.release();
             this.recorderPool.remove(recorderId);
             this.recorderAutoDestroy.remove(recorderId);
+            this.recorderPaused.remove(recorderId);
 
             WritableMap data = new WritableNativeMap();
             data.putString("message", "Destroyed recorder");
@@ -230,6 +232,7 @@ public class AudioRecorderModule extends ReactContextBaseJavaModule implements
         }
 
         this.recorderAutoDestroy.put(recorderId, autoDestroy);
+        this.recorderPaused.put(recorderId, false);
 
         try {
             recorder.prepare();
@@ -249,7 +252,12 @@ public class AudioRecorderModule extends ReactContextBaseJavaModule implements
         }
 
         try {
-            recorder.start();
+            if (this.recorderPaused.get(recorderId)) {
+                recorder.resume();
+                this.recorderPaused.put(recorderId, false);
+            } else {
+                recorder.start();
+            }
 
             callback.invoke();
         } catch (Exception e) {
@@ -297,6 +305,7 @@ public class AudioRecorderModule extends ReactContextBaseJavaModule implements
 
         try {
             recorder.pause();
+            this.recorderPaused.put(recorderId, true);
             callback.invoke();
         } catch (Exception e) {
             callback.invoke(errObj("stopfail", e.toString()));
