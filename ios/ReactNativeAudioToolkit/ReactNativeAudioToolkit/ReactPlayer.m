3ae128124c4cd4316d279ae0ed7a82583525bd7a
Colin McArdell <colinmcardell@gmail.com>
2022-08-27 14:38:28 -0400

    Adds `duration` and `position` to `ReactPlayer`

--- ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/ReactPlayer.m
+++ ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/ReactPlayer.m
@@ -22,4 +22,28 @@
     return self;
 }
 
+- (NSNumber *)duration {
+    NSNumber *duration = @0;
+    
+    if (self.currentItem != nil && self.currentItem.asset != nil) {
+        if (CMTIME_IS_INDEFINITE(self.currentItem.asset.duration) || CMTIME_IS_INVALID(self.currentItem.asset.duration)) {
+            duration = @(-1);
+        } else {
+            duration = @(CMTimeGetSeconds(self.currentItem.asset.duration) * 1000);
+        }
+    }
+    
+    return duration;
+}
+
+- (NSNumber *)position {
+    if (CMTIME_IS_INDEFINITE(self.currentTime) || CMTIME_IS_INVALID(self.currentTime)) {
+        return @(-1);
+    } else {
+        return @(CMTimeGetSeconds(self.currentTime) * 1000);
+    }
+}
+
+
+
 @end
